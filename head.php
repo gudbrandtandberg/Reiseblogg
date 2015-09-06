66799a8ff20246492fd4d154c31962c2a100cef2
Gudbrand <gudbrandduff@gmail.com>
2015-09-06 15:45:06 +0800

    m

--- head.php
+++ head.php
@@ -13,8 +13,8 @@
         $bootstrap_css = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css";
         $bootstrap_js = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js";
         $jquery = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
-        $bootstrap_image_css = "https://github.com/blueimp/Bootstrap-Image-Gallery/css/bootstrap-image-gallery.min.css"; //!!
-        $bootstrap_image_js = "https://github.com/blueimp/Bootstrap-Image-Gallery/js/bootstrap-image-gallery.min.js"; //!!
+        $bootstrap_image_css = "Bootstrap-Image-Gallery-3.1.3/css/bootstrap-image-gallery.min.css"; //!!
+        $bootstrap_image_js = "Bootstrap-Image-Gallery-3.1.3/js/bootstrap-image-gallery.min.js"; //!!
         $blueimp_css = "http://blueimp.github.io/Gallery/css/blueimp-gallery.min.css";
         $blueimp_jquery_js = "http://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js";
     }
